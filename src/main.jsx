54b4f006b6aae3d9a4bc09e0c4fe1256f27ef065
Mohiuddin10 <sj.ctg6737@gmail.com>
2024-12-16 12:28:34 +0600

    working with router and test code push

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/home/Home.jsx';
+import Test from './components/home/Test.jsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/test",
+    element: <Test />
+  },
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
